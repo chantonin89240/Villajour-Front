dcf870492b99200500830e39ef9371f8858c55c8
Kriqx <jeanchristophe.pouzin@diiage.org>
2024-05-23 13:17:51 +0200

    Ajout de radzen, mise en place du scheduler et de la saisie des horarires (juste pop up) actuellement mock api

---
+++ VillajourFrontend/Entity/Appointment.cs
@@ -0,0 +1,15 @@
+﻿namespace VillajourFrontend.Entity
+{
+    public class Appointment
+    {
+        public string Id { get; set; }
+        public int UserID { get; set; }
+        public int MairieId { get; set; }
+        public DateTime DateStart { get; set; }
+        public DateTime DateEnd { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public bool Validation { get; set; }
+        public int AppointmentTypeId { get; set; }
+    }
+}
